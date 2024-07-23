0c72c2bce17fb0ba9d029893247a6af0d910ab73
dkgin <157084404+dkgin@users.noreply.github.com>
2024-07-23 10:58:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -65,8 +65,8 @@ def sendToLine(lineToken):
 
 #schedule.every(20).seconds.do(sendToLine,lineToken) # 20秒跑一次
 
-schedule.every().day.at("19:55").do(sendToLine, lineToken)
-schedule.every().day.at("19:56").do(sendToLine, lineToken)
+schedule.every().day.at("03:00").do(sendToLine, lineToken)
+schedule.every().day.at("03:01").do(sendToLine, lineToken)
 
 def run_schedule():
     while True:
